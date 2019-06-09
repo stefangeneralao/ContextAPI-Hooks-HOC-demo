387ba5cbfd622d6fb3b2e6b0c54290aa8fad7344
Stefan Generalao <stefan.generalao@gmail.com>
2019-06-09 19:30:16 +0200

    Created header and header settings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,25 @@ import Counter from 'components/Counter/Counter';
 import UrbanDictionary from 'components/UrbanDictionary/UrbanDictionary';
 import DateTime from 'components/DateTime/DateTime'
 import TodoList from 'components/TodoList/TodoList';
+import Header from 'components/Header/Header';
+import HeaderSettings from 'components/HeaderSettings/HeaderSettings';
 import { provideCounter } from 'contexts/CounterContext';
 import { provideDateTime } from 'contexts/DateTimeContext';
 import { provideUrbanDictionary } from 'contexts/UrbanDictionaryContext';
 import { provideTodos } from 'contexts/TodosContext';
+import { provideHeaderStyle } from 'contexts/HeaderStyleContext';
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    <Counter />
-    <UrbanDictionary />
-    <DateTime />
-    <TodoList />
+    <Header />
+    <main>
+      <HeaderSettings />
+      <Counter />
+      <UrbanDictionary />
+      <DateTime />
+      <TodoList />
+    </main>
   </div>
 );
 
@@ -24,4 +31,5 @@ export default compose(
   provideCounter,
   provideDateTime,
   provideTodos,
+  provideHeaderStyle,
 )(App);
